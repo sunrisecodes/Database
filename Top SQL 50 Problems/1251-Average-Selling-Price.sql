c2f0b1560cd2d0a0d1df6bf18e45c78d22bbd93e
Shrouk Ahmed <45500394+sunrisecodes@users.noreply.github.com>
2025-02-17 23:55:38 +0200

    [1251] [Time Beats: 83.70%] - LeetPush

--- Top SQL 50 Problems/1251-Average-Selling-Price.sql
+++ Top SQL 50 Problems/1251-Average-Selling-Price.sql
@@ -2,7 +2,7 @@ SELECT
     p.product_id,
     ROUND
         (IFNULL(sum(CASE WHEN u.purchase_date between p.start_date and p.end_date then p.price * u.units end)
-            / ifNULL(sum(CASE WHEN u.purchase_date between p.start_date and p.end_date then u.units end ), 0), 0), 2
+            /sum(CASE WHEN u.purchase_date between p.start_date and p.end_date then u.units end ), 0), 2
         ) as average_price
 FROM Prices p
 LEFT JOIN UnitsSold u
